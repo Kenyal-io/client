7e322c7e0e93741483639900e3fe08e57c19ab47
azputra <ahmadzp102@gmail.com>
2020-01-24 09:30:38 +0700

    add form name add validate name 5 - 12

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <chatForm></chatForm>
+  <div v-show="submit">
+    <div class="row justify-content-center" style="margin-right: 0px; margin-left: 0px;">
+      <div class="col-lg-4">
+        <img src="../assets/dice.gif" alt="dice" style="max-width: 400px;margin-right: 350px;" />
+      </div>
+      <div class="col-lg-4">
+        <formName @newUser="userJoin"></formName>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import chatForm from "@/components/chatForm.vue";
+import FormName from "@/components/formName.vue";
 
 export default {
   name: "home",
+  data() {
+    return {
+      submit: true
+    };
+  },
   components: {
-    chatForm
+    FormName
+  },
+  methods: {
+    userJoin(username) {
+      this.submit = false;
+    }
   }
 };
 </script>
+
+<style scoped>
+.home {
+  background-image: url("https://wallpaperaccess.com/full/482770.jpg");
+}
+</style>
