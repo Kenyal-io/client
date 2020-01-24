c937436e2b47edad47d216bffb35831ec6ddd2ed
azputra <ahmadzp102@gmail.com>
2020-01-24 09:21:48 +0700

    add chat form add chat form send to server and listen server

--- src/components/DiceRoll.vue
+++ src/components/DiceRoll.vue
@@ -1,24 +1,24 @@
 <template>
-  <button class="btn btn-dark btn-lg btn-dice mt-5" v-on:click="">Roll Dice</button>
+  <button class="btn btn-dark btn-lg btn-dice mt-5">Roll Dice</button>
 </template>
 
 <script>
 export default {
-    name: 'DiceRoll',
-    data() {
-        return {
-            d
-        }
-    }
-}
+  name: "DiceRoll",
+  data() {
+    return {
+      d
+    };
+  }
+};
 </script>
 
 <style>
 .btn-dice {
-    font-weight: bold !important;
+  font-weight: bold !important;
 }
 
 .btn-dice:hover {
-    box-shadow: 3px 3px 3px #808080	;
+  box-shadow: 3px 3px 3px #808080;
 }
 </style>
